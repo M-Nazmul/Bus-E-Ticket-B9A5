e2b9a70981117f2f44fc60492469edf574392b3d
M-Nazmul <najmulni839@gmail.com>
2024-02-19 22:52:19 +0600

    added select seat function

---
+++ JS/select.js
@@ -0,0 +1,18 @@
+var totalCount = 16;
+        var selectedCount = 0;
+
+        function buttonClick(button) {
+            if (button.classList.contains('bg-green-500')) {
+                button.classList.remove('bg-green-500', 'text-black', 'hover:bg-green-500');
+                selectedCount--;
+            } else {
+                if (selectedCount < 4) {
+                    button.classList.add('bg-green-500', 'text-white', 'hover:bg-green-500');
+                    selectedCount++;
+                } else {
+                    alert("You can only select up to 4 seat.");
+                }
+            }
+            document.getElementById("selectedCount").innerText = selectedCount;
+            document.getElementById("totalCount").innerText = totalCount - selectedCount;
+        }
